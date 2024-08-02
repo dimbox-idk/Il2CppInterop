c525f98434516f7f27f9bba6148a211b95529479
Trevor <me@trev.app>
2024-08-02 15:33:50 -0400

    Rewrite JumpTargets
    Fixes the very volatile nature of it before like it working with BONELAB but not my test app

--- Il2CppInterop.Common/XrefScans/XrefScannerLowLevel.cs
+++ Il2CppInterop.Common/XrefScans/XrefScannerLowLevel.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using Disarm;
 
 namespace Il2CppInterop.Common.XrefScans;
@@ -7,70 +6,21 @@ public static class XrefScannerLowLevel
 {
     public static IEnumerable<IntPtr> JumpTargets(IntPtr codeStart, bool ignoreRetn = false, int instructions = 250)
     {
-        return JumpTargetsImpl(XrefScanner.DecoderForAddress(codeStart, instructions * 4), ignoreRetn);
-    }
+        var decoder = XrefScanner.DecoderForAddress(codeStart, instructions * 4);
 
-    private static IEnumerable<IntPtr> JumpTargetsImpl(IEnumerable<Arm64Instruction> myDecoder, bool ignoreRetn)
-    {
-        var firstFlowControl = true;
-
-        foreach (Arm64Instruction instruction in myDecoder)
+        foreach (var instruction in decoder)
         {
-            if ((instruction.MnemonicCategory.HasFlag(Arm64MnemonicCategory.Return) || instruction.Mnemonic == Arm64Mnemonic.RET) && !ignoreRetn)
+            if (instruction.Mnemonic == Arm64Mnemonic.RET && !ignoreRetn)
                 yield break;
 
-            // BL and BLR are calls; B and BR are unconditional branches (B can have a CC qualifier, but the additional check fixes that)
-            // BC is a conditional branch, but adding it fixes a weird bug where it causes Pass 16 to take multiple minutes rather than seconds
-            if (instruction.Mnemonic is Arm64Mnemonic.BL or Arm64Mnemonic.BLR or Arm64Mnemonic.B or Arm64Mnemonic.BR or Arm64Mnemonic.BC && instruction.MnemonicConditionCode == Arm64ConditionCode.NONE)
+            var jump = instruction.Mnemonic is Arm64Mnemonic.B or Arm64Mnemonic.BC or Arm64Mnemonic.BR;
+            var call = instruction.Mnemonic is Arm64Mnemonic.BL or Arm64Mnemonic.BLR;
+
+            if ((jump || call) && instruction.MnemonicConditionCode == Arm64ConditionCode.NONE && instruction.FinalOpConditionCode == Arm64ConditionCode.NONE)
             {
                 var target = XrefScanUtilFinder.ExtractTargetAddress(instruction);
                 yield return (IntPtr)target;
-
-                if (target == 0) yield break;
-
-                //if (firstFlowControl && instruction.Mnemonic is Arm64Mnemonic.B or Arm64Mnemonic.BR) // if unconditional
-                //    yield break;
             }
-
-            if (XrefScanUtilFinder.HasGroup(instruction.Mnemonic, "AArch64_GRP_JUMP") || XrefScanUtilFinder.HasGroup(instruction.Mnemonic, "AArch64_GRP_CALL"))
-                firstFlowControl = false;
         }
     }
-
-    // in the wise words of sircoolness, when porting this, i don't know what it does
-    /*public static IEnumerable<IntPtr> CallAndIndirectTargets(IntPtr pointer)
-    {
-        return CallAndIndirectTargetsImpl(XrefScanner.DecoderForAddress(pointer, 1024 * 1024));
-    }
-
-    private static IEnumerable<IntPtr> CallAndIndirectTargetsImpl(Decoder decoder)
-    {
-        while (true)
-        {
-            decoder.Decode(out var instruction);
-            if (decoder.LastError == DecoderError.NoMoreBytes) yield break;
-
-            if (instruction.FlowControl == FlowControl.Return)
-                yield break;
-
-            if (instruction.Mnemonic == Mnemonic.Int || instruction.Mnemonic == Mnemonic.Int1)
-                yield break;
-
-            if (instruction.Mnemonic == Mnemonic.Call || instruction.Mnemonic == Mnemonic.Jmp)
-            {
-                var targetAddress = XrefScanner.ExtractTargetAddress(instruction);
-                if (targetAddress != 0)
-                    yield return (IntPtr)targetAddress;
-                continue;
-            }
-
-            if (instruction.Mnemonic == Mnemonic.Lea)
-                if (instruction.MemoryBase == Register.RIP)
-                {
-                    var targetAddress = instruction.IPRelativeMemoryAddress;
-                    if (targetAddress != 0)
-                        yield return (IntPtr)targetAddress;
-                }
-        }
-    }*/
 }
